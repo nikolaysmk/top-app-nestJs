b4ed1d0d955640e9b893a214950657ec67b99eae
Nikolay Smk <nikolaysmyk@icloud.com>
2023-04-17 19:54:56 +0300

    init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
+      {/* The lang attribute is needed for accessibility */}
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <main>
+      <h1 className="">About</h1>
+    </main>
+  );
+}
